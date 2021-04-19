a81a214d2784bd24531ed3a0c7acb220d7160b7d
João Lima <jmflima@gmail.com>
2021-04-19 02:29:00 -0300

    Aula-02 - Rotas (usando json-server e axios para request )

--- todolist/src/pages/TaskLists.js
+++ todolist/src/pages/TaskLists.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PageHeader } from "antd";
+import axios from "axios";
+
 const TaskLists = () => {
+    const[list, setList] = useState([]);
+
+    async function getLists() {
+        const res = await axios.get("http://localhost:3004/lists");
+        setList(res.data);
+    }
+
+    useEffect(() => {
+        getLists();
+    }, [])
+
     return (
         <>
            <PageHeader
                 title="Listas"
                 subTitle="Minhas Listas de Tarefas"
-            />     
+            /> 
+
+            { JSON.stringify(list) };    
         </>
     )
 }
